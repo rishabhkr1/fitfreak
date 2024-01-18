f7e23b06477948a67cc9e60d4b550a8b8dc2bd8f
Rishabh Kumar <110474658+rishabhkr1@users.noreply.github.com>
2024-01-18 21:05:20 +0530

    first commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+"use client"
+import React from 'react'
+import logo from '@/assets/logo.png'
+import { IoIosBody } from 'react-icons/io'
+import './Navbar.css'
+import Image from 'next/image'
+import Link from 'next/link'
+import AuthPopup from '../AuthPopup/AuthPopup'
+const Navbar = () => {
+    const [isloggedin, setIsloggedin] = React.useState<boolean>(false)
+
+    const [showpopup , setShowpopup] = React.useState<boolean>(false)
+    return (
+        <nav>
+            <Image src={logo} alt="Logo" />
+            <Link href='/'>Home</Link>
+            <Link href='/about'>About</Link>
+            <Link href='/profile'><IoIosBody/></Link>
+           {
+            isloggedin ? 
+            <button>Logout</button>
+            :
+            <button
+            onClick={()=>{
+                setShowpopup(true)
+            }}
+            >Login</button>
+
+
+            
+           }
+
+           {
+            showpopup && <AuthPopup/>
+           }
+        </nav>
+    )
+}
+
+export default Navbar
